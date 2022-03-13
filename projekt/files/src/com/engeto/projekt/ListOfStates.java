32949041cc1aac9bf5079ee3e15b1076dfe01272
EgorZ <egorr@seznam.cz>
2022-03-13 18:11:54 +0100

    Vytvoření třídy StateException a přidání metod pro List

--- projekt/files/src/com/engeto/projekt/ListOfStates.java
+++ projekt/files/src/com/engeto/projekt/ListOfStates.java
@@ -1,8 +1,25 @@
 package com.engeto.projekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListOfStates {
 
     private static final String DELIMITER = "\t";
 
-    ListOfStates listOfStates = new ListOfStates();
+    List<State> ListOfStates = new ArrayList<>();
+
+    public void addCountry(State state) {
+        ListOfStates.add(state);
+    }
+
+    public State getState(int index) {
+        return ListOfStates.get(index);
+    }
+
+    public void removeState(int index) {
+        ListOfStates.remove(index);
+    }
+
+
 }
---
+++ projekt/files/src/com/engeto/projekt/StateException.java
@@ -0,0 +1,8 @@
+package com.engeto.projekt;
+
+public class StateException extends Exception{
+    public StateException(String error) {
+        super(error);
+    }
+
+}
